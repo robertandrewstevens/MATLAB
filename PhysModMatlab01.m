c4b67e0682a0a5602bf13e351a954cae893e28b4
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2014-04-24 20:03:43 -0500

    Create PhysModMatlab01.m

---
+++ PhysModMatlab01.m
@@ -0,0 +1,32 @@
+% Physical Modeling in MATLAB by Allen B. Downey
+% Chapter 1
+
+% 1.1 A glorified calculator
+
+2 + 1
+
+1+2+3+4+5+6+7+8+9
+
+2*3 - 4/5
+
+2 * (3-4) / 5
+
+2^16
+
+% 1.2 Math functions
+
+sin(1)
+
+atan2(1,1)
+
+exp(1)
+
+log(exp(3))
+
+sqrt(sin(0.5)^2 + cos(0.5)^2)
+
+% 1.3 Documentation
+
+help sin
+
+SIN(1)
