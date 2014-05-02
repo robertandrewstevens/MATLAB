1af48a550040b6aeb16797bbe04ddda7b89c33fc
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2014-05-02 07:49:07 -0500

    Create fibanocci1.m

---
+++ fibanocci1.m
@@ -0,0 +1,12 @@
+% Physical Modeling in MATLAB by Allen B. Downey
+% Chapter 2:  Scripts
+% Exercise 2.1
+
+% Computes the nth Fibonacci number.
+% Precondition: you must assign a value to n before running this script. 
+% Postcondition: the result is stored in ans.
+
+t1 = (1 + sqrt(5)) / 2;
+t2 = (1 - sqrt(5)) / 2;
+diff = t1^n - t2^n;
+ans = diff / sqrt(5)
