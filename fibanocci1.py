de8738be7383ab67f1ff4540964f318560501dc7
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2014-05-02 13:23:29 -0500

    Update fibanocci1.py

--- fibanocci1.py
+++ fibanocci1.py
@@ -6,7 +6,7 @@
 # Precondition: you must assign a value to n before running this script. 
 # Postcondition: the result is stored in ans.
 
-
+from math import *
 
 t1 = (1.0 + sqrt(5.0)) / 2.0
 t2 = (1.0 - sqrt(5.0)) / 2.0
