eb4a98fc03e9fe16a83ad82d2195ab65424c04e1
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2014-05-05 22:04:42 -0500

    Update PhysModMatlab02.py

--- PhysModMatlab02.py
+++ PhysModMatlab02.py
@@ -1,14 +1,16 @@
 # Physical Modeling in MATLAB by Allen B. Downey
 # Chapter 2:  Scripts
-# MATLAB script refactored into Python (via R) - WIP
+# MATLAB script refactored into Python (via R)
 # comments are indicted by "#" instead of "%"
 # assignments use "<-" instead of "=" (although it's allowed, it is not recommneded)
 
 from math import *
+import myscript
+#import fibanocci1
 
 # 2.1 M-files
 
-source("myscript.R", echo = TRUE)
+myscript
 
 #my script # comment out so script completes without (intentional) error
 
@@ -21,12 +23,20 @@ source("myscript.R", echo = TRUE)
 x = 5
 y = 7
 z = 9
-objects() # "who" in MATLAB
+# "who" in MATLAB or objects() in R
+# http://www.gossamer-threads.com/lists/python/python/565579
+print "Your variables are:"
+print [var for var in dir() if not (var.startswith('_') or var == 'var')]
+print ""
 
-rm(y) # "clear y" in MATLAB
-objects()
+# http://grokbase.com/t/python/tutor/07adweedqr/variables-in-workspace
+del y # "clear y" in MATLAB or rm(y)  in R
 
-print "z = ", z # "disp(z)" in MATLAB
+print "Your variables are:"
+print [var for var in dir() if not (var.startswith('_') or var == 'var')]
+print ""
+ 
+print ">> z = ", z # "disp(z)" in MATLAB
 print ""
 
 # 2.4 More errors
@@ -36,24 +46,28 @@ print ""
 #fibanocci1 # comment out so script completes without (intentional) error
 
 n = 10
-source("fibanocci1.R", echo = TRUE)
+# http://stackoverflow.com/questions/5788891/execfile-with-argument-in-python-shell
+import sys
+sys.argv = ['fibanocci1.py','n']
+execfile('fibanocci1.py')
 
 # 2.5 Pre- and post-conditions
 
-#help("fibanocci1.R") # comment out - help is for commands or packages
+#help("fibanocci1.R") # comment out - 'help' is for commands or packages in R
+#doc fibanocci1 # comment out - 'doc' is for internal fucntions in Python
 
 # 2.6 Assignment and equality
 
 y = 1
 x = y + 1
-print "x = ", x
+print ">> x = ", x
 print ""
 
 #y + 1 = x % comment out so script completes without (intentional) error
 
 y = 1
 y = y + 1
-print "y = ", y
+print ">> y = ", y
 print ""
 
 # 2.7 Incremental development
@@ -62,11 +76,11 @@ print ""
 
 # 2.8 Unit testing
 
-print "asin(0) = ", asin(0.0)
+print ">> asin(0) = ", asin(0.0)
 print ""
 
-print "asin(1) = ", asin(1.0)
+print ">> asin(1) = ", asin(1.0)
 print ""
 
-print "asin(1) / pi", asin(1.0) / pi
+print ">> asin(1) / pi = ", asin(1.0) / pi
 print ""
