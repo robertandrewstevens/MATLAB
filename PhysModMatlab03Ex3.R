21d0d740869357529e2c0fd845e23081e3e9dc49
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2014-05-16 20:58:30 -0500

    Update PhysModMatlab03Ex3.R

--- PhysModMatlab03Ex3.R
+++ PhysModMatlab03Ex3.R
@@ -1,12 +1,8 @@
 # Physical Modeling in MATLAB by Allen B. Downey
 # Chapter 3:  Loops
-# Script for Exercise 3.3
+# Exercise 3.3
 # MATLAB script refactored into R
 
-# Use a loop to compute elements of A directly:  A(i) = A1*r^(i − 1)
+# Write a script named sequence that uses a loop to compute elements of A directly.
 
-a1 <- 1
-r <- 1/2
-for(i in 2:10) {
-    print(a <- a1*r^(i - 1))
-}
+source("sequence.R")
