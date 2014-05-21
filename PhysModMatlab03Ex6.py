c03060147515295fc158474ba2f7534de4ad9a03
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2014-05-20 21:25:54 -0500

    Create PhysModMatlab03Ex6.py

---
+++ PhysModMatlab03Ex6.py
@@ -0,0 +1,10 @@
+# Physical Modeling in MATLAB by Allen B. Downey
+# Chapter 3:  Loops
+# Exercise 3.6
+# MATLAB script refactored into Pyton (via R)
+
+# Write a script named fib_plot that loops i through a range from 1 to 20, 
+# uses fibonacci2 to compute Fibonacci numbers, and plots Fi for each i with
+# a series of red circles.
+
+execfile("fib_plot.py")
