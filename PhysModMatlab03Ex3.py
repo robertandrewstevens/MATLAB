9c5e6dfb41d4de29e0eec22014e1a95c14261eeb
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2014-05-20 20:39:51 -0500

    Create PhysModMatlab03Ex3.py

---
+++ PhysModMatlab03Ex3.py
@@ -0,0 +1,8 @@
+# Physical Modeling in MATLAB by Allen B. Downey
+# Chapter 3:  Loops
+# Exercise 3.3
+# MATLAB script refactored into Pyton (via R)
+
+# Write a script named sequence that uses a loop to compute elements of A directly.
+
+execfile("sequence.py")
