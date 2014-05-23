099c1f51d7fceb6fe15254a72d5f863f9f717d2c
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2014-05-23 17:18:45 -0500

    Update PhysModMatlab04Ex2.R

--- PhysModMatlab04Ex2.R
+++ PhysModMatlab04Ex2.R
@@ -1,12 +1,12 @@
-% Physical Modeling in MATLAB by Allen B. Downey
-% Chapter 4:  Vectors
-% Exercise 4.2 
+# Physical Modeling in MATLAB by Allen B. Downey
+# Chapter 4:  Vectors
+# Exercise 4.2 
 # MATLAB script refactored into R
 
-% Write a similar loop that multiplies all the elements of a vector
-% together. 
-% You might want to call the accumulator product, and you might want
-% to think about the initial value you give it before the loop.
+# Write a similar loop that multiplies all the elements of a vector
+# together. 
+# You might want to call the accumulator product, and you might want
+# to think about the initial value you give it before the loop.
 
 X <- 1:5
 
