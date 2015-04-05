a726e61c4135bf163a51eabfd2864f091a7737b2
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2015-04-05 18:39:07 -0500

    update
    edited and deleted files

--- PhysModMatlab03Ex3.R
+++
@@ -1,8 +0,0 @@
-# Physical Modeling in MATLAB by Allen B. Downey
-# Chapter 3:  Loops
-# Exercise 3.3
-# MATLAB script refactored into R
-
-# Write a script named sequence that uses a loop to compute elements of A directly.
-
-source("sequence.R")
