ae74ef93ab769a588c2f34777bcc7357bdfd1eb6
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2018-06-03 21:49:46 -0500

    update
    remove unneeded files

--- isintegral.m
+++
@@ -1,7 +0,0 @@
-function res = isintegral(x)
-    if round(x) == x
-    res = 1;
-    else
-        res = 0;
-    end
-end
--- myfunc.m
+++
@@ -1,9 +0,0 @@
-% res = myfunc (x)
-% Compute the Manhattan distance from the origin to the
-% point on the unit circle with angle (x) in radians.
-
-function res = myfunc(x)
-    s = sin(x)
-    c = cos(x)
-    res = abs(s) + abs(c)
-end
